e250839851425f55f61bffdf680da959874ce5eb
Tamzin Hadasa Kelly <tamzin@tamz.in>
2021-07-22 12:47:07 -0700

    update documentation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import patrol.rfd
+"""Runs 'zinbot"""
+__author__ = "Tamzin Hadasa Kelly"
+__copyright__ = "Copyright 2021, Tamzin Hadasa Kelly"
+__license__ = "The MIT License"
+__email__ = "coding@tamz.in"
+
+import patrol.newpages
 
 def run():
-    patrol.rfd.checkqueue()
+    """Run the bot's tasks that are currently approved / in trial"""
+    patrol.newpages.checkqueue()  # pre-trial
 
 if __name__ == "__main__":
+    print("RUNNING")
     run()
