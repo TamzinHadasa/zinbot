9212cef5bc9af84af9e56c0731abdc926d664cec
Tamzin Hadasa Kelly <coding@tamz.in>
2024-10-07 16:24:32 -0400

    show date in logging

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 """Runs 'zinbot."""
 __author__ = "Tamzin Hadasa Kelly"
-__copyright__ = "Copyright 2021–2024, Tamzin Hadasa Kelly"
+__copyright__ = "Copyright 2021-2024, Tamzin Hadasa Kelly"
 __license__ = "The MIT License"
 __email__ = "coding@tamz.in"
 __version__ = "1.4.4"
 
 import time
 
+import api
 import pagetriage.newpages
 
 
 def run() -> None:
     """Run the bot's tasks."""
+    print(f"Site time is {api.site_time()}")
     pagetriage.newpages.checkqueue()
 
 
@@ -19,5 +21,5 @@ if __name__ == "__main__":
     print(f"RUNNING (version {__version__})")
     while True:
         run()
-        print("Run done. Sleeping.")
+        print(f"{api.site_time()}: Run done. Sleeping.")
         time.sleep(1800)
