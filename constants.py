57950e02754d59fb716aab407fa9341880c2e98d
Tamzin Hadasa Kelly <coding@tamz.in>
2024-10-14 02:18:37 -0400

    simplify RfD check maintenance costs outweigh minimal antivandalism benefits

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@
 
 API_URL = "https://en.wikipedia.org/w/api.php?"
 WIKI_URL = "https://en.wikipedia.org/wiki/"
-VERSION = "2.0.0"
+VERSION = "2.0.0-simplified-rfd-check-0"
 USER_AGENT = f"'zinbot ({VERSION}) wikimedian@tamz.in"
