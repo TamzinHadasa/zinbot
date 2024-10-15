b78acabb5d3a2aac7280fdc6743b44bc1c1f831b
Tamzin Hadasa Kelly <coding@tamz.in>
2024-10-14 22:32:09 -0400

    increment version

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@
 
 API_URL = "https://en.wikipedia.org/w/api.php?"
 WIKI_URL = "https://en.wikipedia.org/wiki/"
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 USER_AGENT = f"'zinbot ({VERSION}) wikimedian@tamz.in"
