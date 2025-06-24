92fc3805fec538b9c9d8f72983245b3ccb2d9693
Tamzin Hadasa Kelly <coding@tamz.in>
2025-06-24 18:07:36 +0700

    Sammy says this is a better user agent

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@
 API_URL = "https://en.wikipedia.org/w/api.php?"
 WIKI_URL = "https://en.wikipedia.org/wiki/"
 VERSION = "2.2.1"
-USER_AGENT = f"'zinbot ({VERSION}) wikimedian@tamz.in"
+USER_AGENT = f"*You* should commit API crimes -- TheresNoTime"
